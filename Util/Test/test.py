e7b12469e87073414c1ba1a34b4991f7d703d4e0
chungchris <imchungchris@hotmail.com>
2019-12-01 18:11:16 +0800

    fix test script

--- Util/Test/test.py
+++ Util/Test/test.py
@@ -6,13 +6,21 @@ Created on Sat Nov 30 12:20:04 2019
 @author: chungchris
 """
 
+import argparse
+import math
+import os
+import random
 import sys
+
+from multiprocessing import Process
+
 sys.path.append("../../Client")
+
 import gui
 import settings
 
+#####
 
-import argparse
 parser = argparse.ArgumentParser(description='Start WSS Chatroom test.')
 parser.add_argument('-p', '--port', dest='port', default=settings.DEFAULT_WSS_PORT, \
                     help=f'server at localhost port. default set at {settings.DEFAULT_WSS_PORT}')
@@ -20,25 +28,16 @@ parser.add_argument('-u', '--users', dest='users', default=3, type=int, \
                     help=f'simulate how many users')
 parser.add_argument('-f', '--frequency', dest='frequency', default=5, type=int, \
                     help=f'e.g. 5: simulate each user to send msg by frequency every t seconds. t will be a random value from 1 to 5.')
-parser.add_argument('-a', '--amount-of-msg', dest='amount', default=20, type=int, \
+parser.add_argument('-a', '--amount-of-msg', dest='amount', default=50, type=int, \
                     help=f'amount of messages each user will send')
 args, unknown = parser.parse_known_args()
 
-# start a server process
-'''
-import subprocess
-try:
-    subprocess.run(['python3.7','wss_server.py','-u','5566','-p',f'{args.port}','-d','0'])
-except Exception as e:
-    print(f'run server error:{e}')
-    sys.exit(0)
-'''
-import os
-from multiprocessing import Process
+#####
 
+# start a server process
+def runServer():
+    os.system(f'python3.7 ../../Server/wss_server.py -u 5566 -p {args.port}')
 try:
-    def runServer():
-        os.system(f"python3.7 ../../Server/wss_server.py -u 5566 -p {args.port}")
     p = Process(target=runServer)
     p.start()
 except Exception as e:
@@ -46,20 +45,18 @@ except Exception as e:
     sys.exit(0)
 print(f'### tester ### server started')
 
-# start wss client gui for each user in indep process
+#####
+
 if args.frequency > 1:
     r = (1, args.frequency)
 else:
     r = (1, 5)
-import math
 power = int(math.log(r[1], 10))+2
 
-a = args.amount
+names = []
 nn = len(settings.ALLOWED_CHAR_FOR_NAME)
 ALLOWED_CHAR_FOR_NAME = list(settings.ALLOWED_CHAR_FOR_NAME)
 
-import random
-names = []
 def genName():
     while True:
         name = ''
@@ -85,7 +82,7 @@ def genTest(name):
         f.write(f'{settings.JSON_VALUE_REQUEST_TYPE_LOGIN};{name};5566{n}')
         f.write(f'sleep2{n}')
         # send msgs
-        for _ in range(10):
+        for _ in range(args.amount):
             t = (int(random.random()*(10**power)) % r[1]) + r[0]
             msg = name*t
             if len(msg) > settings.MAX_MSG_LEN:
@@ -96,20 +93,21 @@ def genTest(name):
         f.write(f'{settings.JSON_VALUE_REQUEST_TYPE_UNREG};{name}{n}')
         f.write(f'sleep2{n}')
 
-import time
 def runTest(file_name):
-    print(f'to start a client with tc name:{file_name}')
+    print(f'### tester ### to start a client with tc name:{file_name}')
     gui.wssClientGUI(args.port, test=True, test_case=file_name)
 
 try:
     os.mkdir('Testcase')
 except FileExistsError:
     pass
-
-p_clients = []
 for i in range(args.users):
     name = genName()
     genTest(name)
+print('### tester ### test cases generated under ./Testcase/')
+
+# start wss client gui for each user in indep process
+p_clients = []
 for name in names:
     p = Process(target=runTest, args=('tc_' + name + '.txt',))
     p_clients.append(p)
